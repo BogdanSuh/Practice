8f3602c715f832d9a23e09466d85578250b57f1c
BogdanSuh <sbv.220613@gamil.com>
2026-02-17 14:58:14 +0700

    Создал кнопку и соответствующий метод, который позволяет уменьшать значение, хранящееся в cart.

---
+++ task 5/js/main.js
@@ -0,0 +1,37 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        cart: 0,
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
