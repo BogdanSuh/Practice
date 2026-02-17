50019dd2a9aa4d1b6748f64ced1cf07d25d9d31f
BogdanSuh <sbv.220613@gamil.com>
2026-02-17 17:24:40 +0700

    Добавил свойство onSale в объект с данными, создал вычисляемое свойство sale, которое, на основе brand, product и onSale формирует строку, сообщающую о том, проводится ли сейчас распродажа или нет и вывел эту строку в карточке товара

---
+++ task 7/js/main.js
@@ -0,0 +1,54 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        cart: 0,
+        altText: "A pair of socks",
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
+            }
+        ]
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity > 0;
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + 'is now on sale';
+            } else {
+                return this.brand + ' ' + this.product + 'is not on sale currently';
+            }
+        }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+    }
+})
